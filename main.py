f9873db46bad0dc954989e8626e48dce07cadb2d
Lizard670 <monarcasemtempo@gmail.com>
2025-01-29 21:36:07 -0300

    Adicionada página de adicionar novo produto e página de ver o estoque

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ class InterfaceLoja(Tk):
         # Muda o título da janela
         self.title("Distribuidora")
 
-        # Cria um objeto fonte que pode ser reutilizado para todos os titulos
+        # Cria os objetos fonte que podem ser reutilizados
         self.fonte_titulo = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+        self.fonte_botao = tkfont.Font(family='Helvetica', size=11)
+        self.fonte_tabela = tkfont.Font(family='Helvetica', size=12, weight="bold")
+
+        # Listas do estoque e das vendas
+        self.estoque = []
+        self.vendas = []
+        # Proximo id que vai ser entregado quando criar um novo produto
+        self.proximo_id = 0
+        self.novo_produto(criar_produto("Teste", 15))
 
         # Container que guarda os frames das outras páginas
         # Quando precisar abrir uma página use o método abrir_pagina
@@ -21,21 +30,18 @@ class InterfaceLoja(Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-        # Inicialização das páginas, todas precisam ocupar o mesmo espaço para que fiquem empilhadas
-        self.paginas = {}
-        self.paginas["telaInicial"] = TelaInicial(parent=container, controller=self)
-        self.paginas["telaInicial"].grid(row=0, column=0, sticky="nsew")
+        # Inicialização das páginas
+        self.paginas = {"TelaInicial": TelaInicial(parent=container, controller=self),
+                        "Estoque": Estoque(parent=container, controller=self),
+                        "TelaVerEstoque": TelaVerEstoque(parent=container, controller=self),
+                        "TelaAdicionarProduto": TelaAdicionarProduto(parent=container, controller=self),
+                        "Vendas": Vendas(parent=container, controller=self),
+                        "Estatisticas": Estatisticas(parent=container, controller=self)}
+        # Todas as páginas precisam ocupar o mesmo espaço para que fiquem empilhadas
+        for nome, pagina in self.paginas.items():
+            pagina.grid(row=0, column=0, sticky="nsew")
 
-        self.paginas["estoque"] = Estoque(parent=container, controller=self)
-        self.paginas["estoque"].grid(row=0, column=0, sticky="nsew")
-
-        self.paginas["vendas"] = Vendas(parent=container, controller=self)
-        self.paginas["vendas"].grid(row=0, column=0, sticky="nsew")
-
-        self.paginas["estatisticas"] = Estatisticas(parent=container, controller=self)
-        self.paginas["estatisticas"].grid(row=0, column=0, sticky="nsew")
-
-        self.abrir_pagina("telaInicial")
+        self.abrir_pagina("TelaInicial")
 
     # Abre a página passada
     def abrir_pagina(self, page_name):
@@ -43,25 +49,35 @@ class InterfaceLoja(Tk):
         # Dando um raise na página, ela vai ficar sobre todas as outras e apenas o conteúdo dela vai aparecer na tela
         pagina.tkraise()
 
+    def novo_produto(self, produto):
+        produto["id"] = self.proximo_id
+        self.estoque.append(produto)
+        self.proximo_id += 1  # Atualiza o proximo id
+
 
 class TelaInicial(Frame):
-
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
+        Frame.__init__(self, parent, pady=5)
         self.controller = controller
+
+        # Texto que fica na parte de cima da tela
         label = Label(self, text="Bem vindo ao programa de \ngerenciamento da distribuidora",
                       font=controller.fonte_titulo, pady=10, padx=10)
         label.pack(side="top", fill="x", pady=10)
 
+        # Botões que levam para outras páginas
         self.frameBotoes = Frame(self)
         self.frameBotoes.pack()
 
         botaoEstoque = Button(self.frameBotoes, text="Abrir página \ndo estoque",
-                         command=lambda: controller.abrir_pagina("estoque"))
+                              command=lambda: controller.abrir_pagina("Estoque"),
+                              font=controller.fonte_botao)
         botaoVendas = Button(self.frameBotoes, text="Abrir página \nde vendas",
-                         command=lambda: controller.abrir_pagina("vendas"))
+                             command=lambda: controller.abrir_pagina("Vendas"),
+                             font=controller.fonte_botao)
         botaoEstatisticas = Button(self.frameBotoes, text="Abrir página \nde estatisticas",
-                         command=lambda: controller.abrir_pagina("estatisticas"))
+                                   command=lambda: controller.abrir_pagina("Estatisticas"),
+                                   font=controller.fonte_botao)
 
         botaoEstoque.pack(side="left", padx=5)
         botaoVendas.pack(side="left", padx=5)
@@ -73,11 +89,137 @@ class Estoque(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.controller = controller
-        label = Label(self, text="This is page 1", font=controller.fonte_titulo)
+
+        # Texto do topo da tela
+        label = Label(self, text="Estoque", font=controller.fonte_titulo)
+        label.pack(side="top", fill="x", pady=30)
+
+        # Botões
+        self.frameBotoes = Frame(self)
+        self.frameBotoes.pack()
+
+        botaoVer = Button(self.frameBotoes, text="Ver e editar",
+                          command=lambda: controller.abrir_pagina("TelaVerEstoque"),
+                          width=12, height=2, font=controller.fonte_botao)
+        botaoAdicionar = Button(self.frameBotoes, text="Novo produto",
+                                command=lambda: controller.abrir_pagina("TelaAdicionarProduto"),
+                                width=12, height=2, font=controller.fonte_botao)
+        botaoVoltar = Button(self.frameBotoes, text="Voltar",
+                             command=lambda: controller.abrir_pagina("TelaInicial"),
+                             width=12, height=2, font=controller.fonte_botao)
+
+        botaoVer.pack(side="left", padx=5)
+        botaoAdicionar.pack(side="left", padx=5)
+        botaoVoltar.pack(side="left", padx=5)
+
+
+class TelaVerEstoque(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent, padx=10, pady=10)
+        self.controller = controller
+        self.atributos = ("id", "nome", "preço", "quantidade")
+        self.tamanhos = (5, 20, 8, 11)
+
+        # Botões
+        self.frameBotoes = Frame(self)
+        self.frameBotoes.pack()
+
+        self.botaoSalvar = Button(self.frameBotoes, text="Salvar",
+                                  command=self.salvar,
+                                  width=12, height=2, font=controller.fonte_botao)
+        # TODO perguntar se você quer voltar sem salvar
+        self.botaoVoltar = Button(self.frameBotoes, text="Voltar",
+                                  command=lambda: controller.abrir_pagina("Estoque"),
+                                  width=12, height=2, font=controller.fonte_botao)
+
+        self.botaoSalvar.pack(side="left", padx=5, pady=10)
+        self.botaoVoltar.pack(side="left", padx=5, pady=10)
+
+        # Legendas
+        self.frameLegendas = Frame(self)
+        self.frameLegendas.pack()
+        self.legendas = []
+        for i in range(len(self.atributos)):
+            label = Label(self.frameLegendas, text=self.atributos[i].upper(), width=self.tamanhos[i] - 1,
+                          font=controller.fonte_tabela)
+            self.legendas.append(label)
+            label.pack(side="left")
+
+        # Tabela
+        contLinha = 0
+        self.tabela = Frame(self)
+        for produto in controller.estoque:
+            contColuna = 0
+            for atributo in self.atributos:
+                self.e = Entry(self.tabela, width=self.tamanhos[contColuna],
+                               font=controller.fonte_tabela)
+                self.e.grid(row=contLinha, column=contColuna)
+                self.e.insert(END, produto[atributo])
+                contColuna += 1
+
+            contLinha += 1
+        self.tabela.pack()
+
+    def salvar(self):
+        pass
+
+
+class TelaAdicionarProduto(Frame):
+    # todo
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
+        label = Label(self, text="This is page 2", font=controller.fonte_titulo)
         label.pack(side="top", fill="x", pady=10)
-        button = Button(self, text="Go to the start page",
-                        command=lambda: controller.abrir_pagina("telaInicial"))
-        button.pack()
+
+        # Campos para o usuário preencher
+        self.frameNome = Frame(self)
+        self.labelNome = Label(self.frameNome, text="Nome: ")
+        self.entryNome = Entry(self.frameNome, width=28)
+        self.frameNome.pack(padx=5, pady=5)
+        self.labelNome.pack(side="left")
+        self.entryNome.pack(side="left")
+
+        self.framePrecoEQuantidade = Frame(self)
+        self.labelPreco = Label(self.framePrecoEQuantidade, text="Preço: ")
+        self.entryPreco = Entry(self.framePrecoEQuantidade, width=8)
+        self.labelQuantidade = Label(self.framePrecoEQuantidade, text="Quantidade: ")
+        self.entryQuantidade = Entry(self.framePrecoEQuantidade, width=4)
+        self.entryQuantidade.insert(0, "0")
+        self.framePrecoEQuantidade.pack(padx=5, pady=5)
+        self.labelPreco.pack(side="left")
+        self.entryPreco.pack(side="left")
+        self.labelQuantidade.pack(side="left", padx=(10, 0))
+        self.entryQuantidade.pack(side="left")
+
+        # Botões
+        self.frameBotoes = Frame(self)
+        self.frameBotoes.pack()
+
+        botaoAdicionar = Button(self.frameBotoes, text="Adicionar",
+                                command=self.novo_produto)
+        botaoCancelar = Button(self.frameBotoes, text="Cancelar",
+                               command=lambda: controller.abrir_pagina("Estoque"))
+
+        botaoAdicionar.pack(side="left")
+        botaoCancelar.pack(side="left", padx=(15, 0))
+
+    def novo_produto(self):
+        try:
+            nome = self.entryNome.get()
+            # Checa se possui um nome válido
+            if not nome or not nome.strip():
+                raise ValueError
+
+            preco = int(self.entryPreco.get())
+            quantidade = int(self.entryPreco.get())
+        except ValueError:
+            # todo popup de erro
+            print("Não foi possível adicionar o novo produto: valor invalido")
+            return
+        else:
+            self.controller.novo_produto(criar_produto(nome, preco, quantidade=quantidade))
+            self.controller.abrir_pagina("Estoque")
 
 
 class Vendas(Frame):
@@ -104,45 +246,8 @@ class Estatisticas(Frame):
         button.pack()
 
 
-
-
-class Estabelecimento:
-    def __init__(self):
-        self.estoque = []
-        self.vendas = []
-        # Proximo id que vai ser entregado quando criar um novo produto
-        self.proximo_id = 0
-
-    def novo_produto(self, nome=None, preco=None, quantidade=None):
-        if nome is None:
-            nome = input("Insira o nome do produto: ")
-
-        if preco is None:
-            preco = float(input("Insira o valor: "))
-
-        if quantidade is None:
-            quantidade = int(input("Insira a quantidade inicial: "))
-
-        self.estoque.append(Produto(nome, preco, self.proximo_id, quantidade))
-        self.proximo_id += 1  # Atualiza o proximo id
-
-
-    def editar_produto(self, id_produto):
-        for produto in self.estoque:
-            # Checa se o id é igual ao id do produto que deseja editar
-            if id_produto == produto.id_numerico:
-                pass
-                # todo
-
-
-class Produto:
-    def __init__(self, nome, preco, id_numerico, quantidade=0):
-        self.id_numerico = id_numerico
-        self.nome = nome
-        self.preco = preco
-        self.quantidade = quantidade
-
-        self.ativo = True
+def criar_produto(nome, preco, id_produto=-1, quantidade=0):
+    return {"nome": nome, "preço": preco, "id": id_produto, "quantidade": quantidade}
 
 
 class Venda:
