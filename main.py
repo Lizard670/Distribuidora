197ec9248f4a088a62d49a6022ff5879e507c590
Lizard670 <monarcasemtempo@gmail.com>
2025-02-01 10:39:31 -0300

    Adicionado função temporária que mostra as opções disponíveis para os elementos, auxiliando no uso da lib ttk

--- main.py
+++ main.py
@@ -377,6 +377,38 @@ def criar_venda(produto, quantidade, data, desconto=1):
             "desconto": desconto}
 
 
+def stylename_elements_options(stylename):
+    '''Function to expose the options of every element associated to a widget
+       stylename.'''
+    try:
+        # Get widget elements
+        style = Style()
+        layout = str(style.layout(stylename))
+        print('Stylename = {}'.format(stylename))
+        print('Layout    = {}'.format(layout))
+        elements = []
+        for n, x in enumerate(layout):
+            if x == '(':
+                element = ""
+                for y in layout[n + 2:]:
+                    if y != ',':
+                        element = element + str(y)
+                    else:
+                        elements.append(element[:-1])
+                        break
+        print('\nElement(s) = {}\n'.format(elements))
+
+        # Get options of widget elements
+        for element in elements:
+            print('{0:30} options: {1}'.format(
+                element, style.element_options(element)))
+
+    except tk.TclError:
+        print('_tkinter.TclError: "{0}" in function'
+              'widget_elements_options({0}) is not a regonised stylename.'
+              .format(stylename))
+
+
 if __name__ == "__main__":
     # Cria a interface e roda o loop principal dela
     interface = InterfaceLoja()
